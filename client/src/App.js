5637206479186ca16cc35602569f92a40f7806a9
Patrick Nymark <patricknymark@Patricks-MacBook-Pro.local>
2020-08-11 21:56:53 +0200

    added bases layout including pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+// pages
+import Login from './pages/Login';
+import Chat from './pages/Chat';
+import Register from './pages/Register';
+
+// components
+import Navbar from './components/navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router className="App">
+        <Navbar />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/chat" component={Chat} />
+    </Router>
   );
 }
 
