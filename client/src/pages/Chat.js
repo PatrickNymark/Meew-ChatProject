26199de3930b59cd5f198eca4c4a58d74619c33c
Patrick Nymark <patricknymark@Patricks-MacBook-Pro.local>
2020-08-11 22:42:39 +0200

    finished base layout for chat

--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import '../sass/chat.scss';
 
 export default function Chat() {
     return (
         <div>
-            <h1>Chat Page</h1>
+            <div className="chat-content-wrapper">
+                <div className="chat-sidebar">
+                    <h1 className="title">Chat App</h1>
+                    <p className="subtitle">By Meew Interns</p>
+
+                    <div className="users-list">
+                        <div className="user">
+                            <p>Patrick Nymark</p>
+
+                            <div className="status-star">&#9734;</div>
+                            <div className="status-circle"></div>
+                        </div>
+                    </div>
+                </div>
+                <div className="chat-content">
+                    <h1>Content</h1>
+                    <div className="chat-window-wrapper">
+                        <div className="chat-window">
+
+                
+                            <input className="message-input" placeholder="Write a message..." />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
