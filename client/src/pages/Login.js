3abace21d7dce7ceed081ba6eef89738fd10b9b5
Shehab <sheh0050@stud.kea.dk>
2020-08-12 12:38:59 +0200

    Added styling for login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,22 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import '../sass/login.scss'
 export default function Login() {
     return (
-        <div>
-            <h1>Login Page</h1>
+        <div className="login-wrapper">
+
+            <div className="login-container">
+
+
+                <div className="email">
+                    <h1>Login Page</h1>
+
+                    <input className="input" type="text" placeholder="Email address" name="email" />
+                    <input className="input" type="text" placeholder="Password" name="password" />
+                    <button className="login-btn">Log In</button>
+                    <p>Not Registered? <Link  className ="click-here" to="/register">Click Here</Link></p>
+                </div>
+            </div>
         </div>
     )
 }
