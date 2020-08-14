3fac7975ea73e3191c073191709ab60c86869ec1
Patrick Nymark <patricknymark@Patricks-MBP.stormchp.com>
2020-08-14 12:15:52 +0200

    small refactor

--- server/server.js
+++ server/server.js
@@ -7,8 +7,6 @@ const io = require('socket.io')(server)
 
 require('dotenv').config();
 
-
-
 mongoose.connect('mongodb://localhost:27017/meew-chat', { 
     useNewUrlParser: true, 
     useUnifiedTopology: true,
@@ -22,68 +20,12 @@ app.use(bodyParser.json())
 app.use('/api/auth', require('./controllers/auth.controller'));
 app.use('/api/users', require('./controllers/user.controller'));
 
-
 const port = process.env.PORT || 5000;
 
 app.use(require('./helpers/error-handler'))
 
 // require socketio listeners
-// require('./services/socket.service')(io)
-
-const Room = require('./models/Room');
-const Message = require('./models/Message');
-const User = require('./models/User')
-
-io.on('connection', socket => {
-    console.log('Server: Connected')
-
-    socket.on('open-room', async ({ userId, loggedUser }) => {
-        // const roomId = userId + loggedUser;
-        const foundRoom = await Room.find({ participants: { $eq: [userId, loggedUser]}});
-        const foundRoom2 = await Room.find({ participants: { $eq: [loggedUser, userId]}});
-        const chatUser = await User.findById(userId);
-
-        console.log(foundRoom.length)
-        console.log(foundRoom2.length)
-
-        if(foundRoom.length == 1 || foundRoom2.length == 1) {
-            console.log('Server: Found Room')
-            let roomId = 0;
-
-            if(foundRoom.length == 1) {
-                roomId = foundRoom[0]._id
-            } else  {
-                roomId = foundRoom2[0]._id
-            }
-
-            socket.join(roomId);
-            socket.emit('room', { roomId, chatUser })
-
-            const messages = await Message.find().where({ roomId }).populate('owner')
-            io.sockets.in(roomId).emit('messages', messages)
-            
-        } else {
-            console.log('Server: New Room')
-
-            const participants = [userId, loggedUser]
-            const newRoom = new Room({ participants });
-            await newRoom.save();
-
-            const roomId = newRoom._id;
-
-            socket.join(roomId);
-            
-            socket.emit('room', { roomId, chatUser });
-        }
-    });
-
-    socket.on('new-message', async ({ message, room, owner }) => {
-        const newMessage = new Message({ message, roomId: room, owner })
-        const savedMessage = await newMessage.save().then(m => m.populate('owner').execPopulate());
-
-        io.sockets.in(room).emit('message', savedMessage)
-    })
-})
+require('./services/socket.service')(io)
 
 
 server.listen(port, () => console.log(`Server running on ${port}`))
--- server/services/socket.service.js
+++ server/services/socket.service.js
@@ -1,45 +1,57 @@
-const Room = require('../models/Room');
-const Message = require('../models/Message');
+const Room = require('./models/Room');
+const Message = require('./models/Message');
+const User = require('./models/User')
+
+module.exports = function(io) {
+    io.on('connection', socket => {
+        console.log('Server: Connected')
+
+        socket.on('open-room', async ({ userId, loggedUser }) => {
+            // const roomId = userId + loggedUser;
+            const foundRoom = await Room.find({ participants: { $eq: [userId, loggedUser]}});
+            const foundRoom2 = await Room.find({ participants: { $eq: [loggedUser, userId]}});
+            const chatUser = await User.findById(userId);
+
+            console.log(foundRoom.length)
+            console.log(foundRoom2.length)
+
+            if(foundRoom.length == 1 || foundRoom2.length == 1) {
+                console.log('Server: Found Room')
+                let roomId = 0;
+
+                if(foundRoom.length == 1) {
+                    roomId = foundRoom[0]._id
+                } else  {
+                    roomId = foundRoom2[0]._id
+                }
+
+                socket.join(roomId);
+                socket.emit('room', { roomId, chatUser })
+
+                const messages = await Message.find().where({ roomId }).populate('owner')
+                io.sockets.in(roomId).emit('messages', messages)
+                
+            } else {
+                console.log('Server: New Room')
+
+                const participants = [userId, loggedUser]
+                const newRoom = new Room({ participants });
+                await newRoom.save();
+
+                const roomId = newRoom._id;
+
+                socket.join(roomId);
+                
+                socket.emit('room', { roomId, chatUser });
+            }
+        });
+
+        socket.on('new-message', async ({ message, room, owner }) => {
+            const newMessage = new Message({ message, roomId: room, owner })
+            const savedMessage = await newMessage.save().then(m => m.populate('owner').execPopulate());
+
+            io.sockets.in(room).emit('message', savedMessage)
+        })
+    })
+}
  
-// module.exports = function(socket) {
-//     socket.on('connection', function (socket) {
-        
-//         socket.on('open-room', async ({ userId, loggedUser }) => {
-
-//             const roomId = userId + loggedUser;
-
-//             const foundRoom = await Room.findOne({ roomId });
-
-//             if(foundRoom) {
-//                 console.log('Server: Found Room')
-
-//                 socket.join(foundRoom.roomId);
-
-//                 // socket.emit('test', 'tese')
-//                 // const messages = await Message.find().where({ roomId });
-
-//                 socket.to(foundRoom.roomId).emit('messages', ['tesst', 'test2', 'test3'])
-                
-//             } else {
-//                 console.log('Server: New Room')
-
-//                 const participants = [userId, loggedUser]
-//                 const newRoom = new Room({ roomId, participants });
-//                 await newRoom.save();
-
-//                 socket.join(roomId);
-                
-//                 socket.emit('room', roomId);
-//             }
-//         });
-
-//         socket.on('new-message', async ({ message, room, owner }) => {
-//             const newMessage = new Message({ message, roomId: room, owner })
-//             await newMessage.save();
-
-//             // socket.emit('message', newMessage)
-
-//             socket.to(room).emit('message', message)
-//         })
-//     })
-// };
